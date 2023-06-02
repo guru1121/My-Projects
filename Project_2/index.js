22141bce7b1d1af41b9f8b2d0796cffbd572aa92
guru1121 <gurunandm@gmail.com>
2023-06-02 08:42:57 +0530

    update

---
+++ Project_2/index.js
@@ -0,0 +1,12 @@
+function myFunc(){
+    
+   if ( document.getElementById("div1") == "div1"){
+    document.getElementById("div1").innerHTML = "this is first div";
+   }
+   else (document.getElementById("div2") == "div2"){
+    document.getElementById("div2").innerHTML = "this is second div";
+   }
+}
+
+
+
